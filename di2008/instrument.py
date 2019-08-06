bd42c38977f43272b1a4a8b66450af70e1fa1dc4
Jason R. Jones <slightlynybbled@gmail.com>
2019-08-06 15:11:14 -0400

    Fixed bug

--- di2008/instrument.py
+++ di2008/instrument.py
@@ -123,8 +123,9 @@ class AnalogPort(Port):
             valid_ranges = [0.01, 0.025, 0.05, 0.1, 0.25, 0.5,
                             1.0, 2.5, 5.0, 10.0, 25.0, 50.0]
             if analog_range not in valid_ranges:
+                strings = [str(v) for v in valid_ranges]
                 raise ValueError('valid values for analog range: '
-                                 f'{", ".join(valid_ranges)}')
+                                 f'{", ".join(strings)}')
 
             if analog_range >= 1.0:
                 configuration |= (1 << self._range_bit)  # set the range bit
